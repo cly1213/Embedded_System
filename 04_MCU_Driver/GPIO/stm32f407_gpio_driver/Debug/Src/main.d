313d1b2d231362f92288035023cfea3f60ab0d84
cly1213 <cly901213@gmail.com>
2021-07-13 21:06:54 +0800

    upload

---
+++ 04_MCU_Driver/GPIO/stm32f407_gpio_driver/Debug/Src/main.d
@@ -0,0 +1,10 @@
+Src/main.o: ../Src/main.c \
+ /Users/leo/Embedded_C/MCU1/target/stm32f407_drivers/drivers/Inc/stm32f407xx.h \
+ /Users/leo/Embedded_C/MCU1/target/stm32f407_drivers/drivers/Inc/stm32f407xx_gpio.h \
+ /Users/leo/Embedded_C/MCU1/target/stm32f407_drivers/drivers/Inc/stm32f407xx.h
+
+/Users/leo/Embedded_C/MCU1/target/stm32f407_drivers/drivers/Inc/stm32f407xx.h:
+
+/Users/leo/Embedded_C/MCU1/target/stm32f407_drivers/drivers/Inc/stm32f407xx_gpio.h:
+
+/Users/leo/Embedded_C/MCU1/target/stm32f407_drivers/drivers/Inc/stm32f407xx.h:
---
+++ 04_MCU_Driver/GPIO/stm32f407_gpio_driver/Debug/drivers/Src/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../drivers/Src/stm32f407xx_gpio.c 
+
+OBJS += \
+./drivers/Src/stm32f407xx_gpio.o 
+
+C_DEPS += \
+./drivers/Src/stm32f407xx_gpio.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+drivers/Src/stm32f407xx_gpio.o: ../drivers/Src/stm32f407xx_gpio.c drivers/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -c -I"/Users/leo/Embedded_C/MCU1/target/stm32f407_drivers/drivers/Inc" -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"drivers/Src/stm32f407xx_gpio.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
