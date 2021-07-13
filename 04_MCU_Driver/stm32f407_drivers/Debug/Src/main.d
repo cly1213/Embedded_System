94946927ebe589b7dd8554415a2cdaac4797d563
cly1213 <cly901213@gmail.com>
2021-07-13 21:07:36 +0800

    upload

--- 04_MCU_Driver/stm32f407_drivers/Debug/Src/main.d
+++
@@ -1,4 +0,0 @@
-Src/main.o: ../Src/main.c \
- /Users/leo/Embedded_C/MCU1/target/stm32f407_drivers/drivers/Inc/stm32f407xx.h
-
-/Users/leo/Embedded_C/MCU1/target/stm32f407_drivers/drivers/Inc/stm32f407xx.h:
