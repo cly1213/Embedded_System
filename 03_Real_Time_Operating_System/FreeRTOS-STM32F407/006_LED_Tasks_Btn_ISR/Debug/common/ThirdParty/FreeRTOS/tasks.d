2181ac1db8df1a30c2e429ab99bb3ab332f710bf
cly1213 <cly901213@gmail.com>
2022-06-07 20:31:56 +0800

    update

---
+++ 03_Real_Time_Operating_System/FreeRTOS-STM32F407/006_LED_Tasks_Btn_ISR/Debug/common/ThirdParty/FreeRTOS/tasks.d
@@ -0,0 +1,40 @@
+common/ThirdParty/FreeRTOS/tasks.o: \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/tasks.c \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/SEGGER/SEGGER.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/Config/Global.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/projdefs.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/portable.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/task.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/list.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/timers.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/task.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/stack_macros.h
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/SEGGER/SEGGER.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/Config/Global.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/projdefs.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/portable.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/task.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/list.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/timers.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/task.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/FreeRTOS/include/stack_macros.h:
---
+++ 03_Real_Time_Operating_System/FreeRTOS-STM32F407/006_LED_Tasks_Btn_ISR/Debug/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.d
@@ -0,0 +1,6 @@
+common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.o: \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.c \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
