e4f762fc6bf2f6eb802ceed600dc0c8d1db2767b
cly1213 <cly901213@gmail.com>
2022-06-10 21:59:42 +0800

    update

--- 03_Real_Time_Operating_System/FreeRTOS-STM32F407/002_LED_tasks/Debug/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.d
+++
@@ -1,6 +0,0 @@
-common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.o: \
- /Users/leo/Embedded_System/03_Real_Time_Operating_System/FreeRTOS-STM32F407/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.c \
- /Users/leo/Embedded_System/03_Real_Time_Operating_System/FreeRTOS-STM32F407/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-/Users/leo/Embedded_System/03_Real_Time_Operating_System/FreeRTOS-STM32F407/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
--- 03_Real_Time_Operating_System/FreeRTOS-STM32F407/002_LED_tasks/Debug/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.d
+++
@@ -1,6 +0,0 @@
-common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.o: \
- /Users/leo/Embedded_System/03_Real_Time_Operating_System/FreeRTOS-STM32F407/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.S \
- /Users/leo/Embedded_System/03_Real_Time_Operating_System/FreeRTOS-STM32F407/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- /Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-/Users/leo/Embedded_System/03_Real_Time_Operating_System/FreeRTOS-STM32F407/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-/Users/leo/Embedded_C/MasteringRTOS/RTOS_workspace2/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
