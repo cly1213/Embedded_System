158f0a837f313fcbbb03d49d85b665ae67d13a5b
cly1213 <cly901213@gmail.com>
2022-06-10 22:22:08 +0800

    update 009

---
+++ 03_Real_Time_Operating_System/FreeRTOS-STM32F407/009_Queue_Tasks/Debug/ThirdParty/FreeRTOS/portable/MemMang/heap_4.d
@@ -0,0 +1,20 @@
+ThirdParty/FreeRTOS/portable/MemMang/heap_4.o: \
+ ../ThirdParty/FreeRTOS/portable/MemMang/heap_4.c \
+ /Users/leo/Workspace/RTOS_workspace/009_Queue_Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ /Users/leo/Workspace/RTOS_workspace/009_Queue_Tasks/ThirdParty/FreeRTOS/include/projdefs.h \
+ /Users/leo/Workspace/RTOS_workspace/009_Queue_Tasks/ThirdParty/FreeRTOS/include/portable.h \
+ /Users/leo/Workspace/RTOS_workspace/009_Queue_Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ /Users/leo/Workspace/RTOS_workspace/009_Queue_Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ /Users/leo/Workspace/RTOS_workspace/009_Queue_Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ /Users/leo/Workspace/RTOS_workspace/009_Queue_Tasks/ThirdParty/FreeRTOS/include/task.h \
+ /Users/leo/Workspace/RTOS_workspace/009_Queue_Tasks/ThirdParty/FreeRTOS/include/list.h
+/Users/leo/Workspace/RTOS_workspace/009_Queue_Tasks/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+/Users/leo/Workspace/RTOS_workspace/009_Queue_Tasks/ThirdParty/FreeRTOS/include/projdefs.h:
+/Users/leo/Workspace/RTOS_workspace/009_Queue_Tasks/ThirdParty/FreeRTOS/include/portable.h:
+/Users/leo/Workspace/RTOS_workspace/009_Queue_Tasks/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+/Users/leo/Workspace/RTOS_workspace/009_Queue_Tasks/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+/Users/leo/Workspace/RTOS_workspace/009_Queue_Tasks/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+/Users/leo/Workspace/RTOS_workspace/009_Queue_Tasks/ThirdParty/FreeRTOS/include/task.h:
+/Users/leo/Workspace/RTOS_workspace/009_Queue_Tasks/ThirdParty/FreeRTOS/include/list.h:
